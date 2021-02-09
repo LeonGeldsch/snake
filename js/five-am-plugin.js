def8b0323967d7455c3ffbd8fb996b5f8f08ac57
LeonGeldsch <leon.geldschlaeger@gmail.com>
2021-02-09 15:50:21 +0100

    Update five-am-plugin.js

--- js/five-am-plugin.js
+++ js/five-am-plugin.js
@@ -1,8 +1,10 @@
 var badgeWrapper = document.querySelector(".badge_wrapper");
 
+var badge = document.querySelector(".badge_wrapper img");
+
 var sectionGrey = document.querySelector(".section.grey");
 
-
+console.log(badge);
 
 window.addEventListener('scroll', function() {
     //console.log(badgeWrapper.getBoundingClientRect().top, sectionGrey.getBoundingClientRect().top);
