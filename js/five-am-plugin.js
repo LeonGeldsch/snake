5991d4e9281cd54b3fd3099a1b43f1acc1422617
LeonGeldsch <leon.geldschlaeger@gmail.com>
2021-02-09 15:33:45 +0100

    .

--- js/five-am-plugin.js
+++ js/five-am-plugin.js
@@ -2,4 +2,8 @@ var badgeWrapper = document.querySelector(".badge_wrapper");
 
 var sectionGrey = document.querySelector(".section.grey");
 
-console.log(badgeWrapper, sectionGrey);
+
+
+window.addEventListener('scroll', function() {
+    console.log(badgeWrapper.getBoundingClientRect().top, sectionGrey.getBoundingClientRect().top);
+});
